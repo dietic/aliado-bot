2e0e73797614197901aa89a61686eead3760a3e1
Diego <dierios93@gmail.com>
2025-04-24 11:44:15 -0500

    chore(webhook): added toUppercase to match category

--- api/webhook.ts
+++ api/webhook.ts
@@ -55,7 +55,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const { data: providers, error } = await db
       .from("Provider")
       .select("firstName,lastName,phone")
-      .contains("categories", [normalizedCategory])
+      .contains("categories", [normalizedCategory.toUpperCase()])
       .ilike("district", district ?? "%")
       .limit(3);
 
