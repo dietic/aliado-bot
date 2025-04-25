32d307e6632506f87a6133ac745997aa3334f4e8
Diego <dierios93@gmail.com>
2025-04-24 19:21:45 -0500

    chore(classifier): again forgot to add message to classifier

--- src/libs/classifier.ts
+++ src/libs/classifier.ts
@@ -14,6 +14,7 @@ export async function classify(text: string): Promise<{
       properties: {
         category: { type: "string" },
         district: { type: "string" },
+        message: { type: "string" },
       },
       required: ["category"],
     },
